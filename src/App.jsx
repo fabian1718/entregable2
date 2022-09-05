a2e3a843da31bfa0f64d35daf20adc03eacb0e33
fabian1718 <fagodu5@gmail.com>
2022-09-05 15:29:28 -0500

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+
 
 function App() {
-  const [latitude, setLatitude] = useState(0)
-  const [longitude, setLongitude] = useState(0)
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [incon, setIncon] = useState("");
+  const [background, setBackground] = useState("");
+  const [tempCelcius, setTempCelcius] = useState(true);
 
   const apiKey = "5879d53253530d398b0f24a36d531e3e";
-  const [coordinates, setCoordinates] = useState({})
+  const [coordinates, setCoordinates] = useState({});
 
   function success(pos) {
     let crd = pos.coords;
@@ -20,29 +24,53 @@ function App() {
   navigator.geolocation.getCurrentPosition(success);
 
   const celcius = (coordinates.main?.temp - 273.15).toFixed(2);
-  
+  const fahrenheit = ((celcius * 9) / 5 + 32).toFixed(2);
+  console.log(`temperatura en farenhey ${fahrenheit}`);
 
-    useEffect(() => {      
-
-      console.log("segundo");
-      console.log(latitude, longitude);
-
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
-      .then((res) => setCoordinates(res.data))
-    
-  },[])
+  useEffect(() => {
+    if (latitude != 0 || longitude != 0) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+        )
+        .then((res) => setCoordinates(res.data));
+    }
+    if (celcius < -90 && celcius < 15) {
+      setIncon("https://c.tenor.com/-gDxa1PE0ucAAAAC/fr%C3%ADo.gif");
+      setBackground('https://img.freepik.com/foto-gratis/fondo-navidad-nieve_1048-3135.jpg?w=2000');
+    }else if (celcius > 15 && celcius < 24){
+      setIncon("https://phoneky.co.uk/thumbs/screensavers/down/nature/tropical_m4hyjhcd.gif");
+      setBackground('https://misistemasolar.com/wp-content/uploads/2018/10/clima-16.jpg');
+    }else{
+      setIncon("https://reportemisiones.com.ar/wp-content/uploads/2017/11/despejadogif.gif");
+      setBackground("https://img.freepik.com/foto-gratis/fondo-mar-playa-vacio_74190-313.jpg?w=2000");
+    }
+    console.log("segundo");
+    console.log(latitude, longitude);
+  }, [latitude, longitude]);
 
   console.log(coordinates);
+
+  const chageTemperature = () => {
+    setTempCelcius(!tempCelcius);
+  };
+
+  document.body.style = `background-image: url(${background})`;
 
   return (
     <div className="App">
       <h1>{coordinates.sys?.country}</h1>
-      <img src="http://openweathermap.org/img/wn/10n@2x.png" alt="" />
-      <div>Ciudad: {coordinates.name}</div>
-      <div>Temperatura: {celcius} °C</div>
-      <button>Change to farenhey</button>
+      <img src={incon} alt="" />
+      <div>City: {coordinates.name}</div>
+      <div>
+        Temperature: {tempCelcius ? celcius : fahrenheit}{" "}
+        {tempCelcius ? "°C" : "°F"}
+      </div>
+      <button onClick={chageTemperature}>
+        Change to {tempCelcius ? "fahrenheit" : "celcius"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
